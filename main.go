a0e98937098f8ae560353fbc2d906580bbd5aefc
abdallah <test@email.com>
2022-01-27 18:32:04 +0100

    oppgave2 kode

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/02AB/modul1-oppgave2/pakke"
+)
+
+func main() {
+	fmt.Println(pakke.TestQuote())
+}
---
+++ pakke/pakke.go
@@ -0,0 +1,9 @@
+package pakke
+
+import "rsc.io/quote"
+
+func TestQuote() string {
+	return quote.Glass() + "\n" + quote.Go() + "\n" + quote.Hello() + "\n" + quote.Opt();
+
+}
+
